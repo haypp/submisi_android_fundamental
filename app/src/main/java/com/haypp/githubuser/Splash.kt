a9238cbe5d6f1c13379b3527d228c2772527d614
Haypp <47301352+haypp@users.noreply.github.com>
2022-10-04 15:50:21 +0700

    Delete unused Suppress
    @Suppress("DEPRECATION") and FLAG_FULLSCREEN

--- app/src/main/java/com/haypp/githubuser/Splash.kt
+++ app/src/main/java/com/haypp/githubuser/Splash.kt
@@ -4,10 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 
-@Suppress("DEPRECATION")
 class Splash : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,10 +13,6 @@ class Splash : AppCompatActivity() {
         supportActionBar?.hide()
         val numb = 3000
 
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
         Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
